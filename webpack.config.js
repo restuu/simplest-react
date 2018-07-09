1437afbbdf29c7426393f4baac384fe4e2f6499f
Dimitri Wahyudiputra <gitmitri@gmail.com>
2018-07-09 14:40:44 +0700

    Add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  entry: [
+    './src/main.js',
+  ],
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'app.bundle.js',
+  },
+  devServer: {
+    inline: true,
+    port: 8080,
+    contentBase: './',
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ]
+      },
+    ],
+  },
+};
